d662205baa21a0ded82755bc3e2ae135ccc379c7
Moises Velasquez <mvelasquez@pictoric.com>
2022-01-10 17:58:40 -0500

    implement mapbox

--- src/components/mapview/MapView.ts
+++ src/components/mapview/MapView.ts
@@ -1,5 +1,7 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import { usePlacesStore } from '@/composables';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
 
 export default defineComponent({
     name:'MapView',
@@ -11,8 +13,25 @@ export default defineComponent({
         const {isLoading, userLocation, isUserLocationReady} = usePlacesStore();
 
         onMounted(()=>{
-            console.log(mapElement.value)
+            // console.log(mapElement.value)
+            if(isUserLocationReady){
+                return initMap();
+            }
+            console.log('No location map yet')
         });
+
+        const initMap = ()=>{
+
+            if(!mapElement.value) throw new Error('Div Element not exits');
+            if(!userLocation.value) throw new Error('User location not exits');
+
+            const map = new mapboxgl.Map({
+                container: mapElement.value, // container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: userLocation.value, // starting position [lng, lat]
+                zoom: 15 // starting zoom
+            });
+        };
 
         return {
             isLoading,
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import mapboxgl from 'mapbox-gl';
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9pdmUiLCJhIjoiY2t5OTk2anU5MDQ0dTJ3azk4MWlpdXc0byJ9.WQXkDvSlGWC4TKXj-ivQ-Q';
+
 if (!navigator.geolocation) {
     throw new Error('Your browser does not support geolocation');
 }
