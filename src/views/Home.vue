4923eb17f5cd7217c37ad11d1c784c225da07b73
Moises Velasquez <mvelasquez@pictoric.com>
2022-05-20 10:32:31 -0500

    updated vue-cli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,20 @@
 <template>
-  <MapView />
-  <MyLocationBtn/>
+	<MapView />
+	<MyLocationBtn />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { usePlacesStore } from '@/composables';
+import { defineComponent } from "vue";
+import { usePlacesStore } from "@/composables";
 
-import MapView from '@/components/mapview/MapView.vue';
-import MyLocationBtn from '@/components/my-location-btn/MyLocationBtn.vue';
-;
-
+import MapView from "@/components/mapview/MapView.vue";
+import MyLocationBtn from "@/components/my-location-btn/MyLocationBtn.vue";
 
 export default defineComponent({
-  name: 'Home',
-  components: { MapView, MyLocationBtn  },
-  setup() {
-    usePlacesStore();
-  }
+	name: "Home",
+	components: { MapView, MyLocationBtn },
+	setup() {
+		usePlacesStore();
+	},
 });
 </script>
